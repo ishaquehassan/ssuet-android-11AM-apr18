4574ecdc02aa135d46c53824834cb633ed37437e
Ishaq Hassan <ishaquehassan@gmail.com>
2018-04-29 13:09:47 +0500

    Class work of April 29 2018

---
+++ 29April2018/src/Sample.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+    var num1:Int = 10
+    var num2:Float = 1.2f
+    var numDouble:Double = 20.0
+    var numLong:Long = 10L
+
+    var hello:Int = 10
+    hello = 20
+
+    val fixedNum:Int = 10
+
+    var myname = "Ishaq"
+
+    var numWithoutType = 10
+
+    var myChr:Char = 'A'
+
+    print(numWithoutType)
+}
