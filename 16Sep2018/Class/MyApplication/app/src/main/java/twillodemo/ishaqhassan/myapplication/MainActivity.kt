655e3d144505a3230c26d5a5316689758574b15d
Ishaq Hassan <ishaquehassan@gmail.com>
2018-09-16 16:17:28 +0500

    Class code of 2018

---
+++ 16Sep2018/Class/MyApplication/app/src/main/java/twillodemo/ishaqhassan/myapplication/MainActivity.kt
@@ -0,0 +1,14 @@
+package twillodemo.ishaqhassan.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.view.ViewGroup
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
