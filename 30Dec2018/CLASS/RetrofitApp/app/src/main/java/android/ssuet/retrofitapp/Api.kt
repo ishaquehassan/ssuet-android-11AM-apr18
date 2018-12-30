839a933e293cea212cdf25dbc7c4a8782db443f7
Ishaq Hassan <ishaquehassan@gmail.com>
2018-12-30 14:10:11 +0500

    Fixed Stetho Network Inspector in 30th DEC 2018 Class Code

--- 30Dec2018/CLASS/RetrofitApp/app/src/main/java/android/ssuet/retrofitapp/Api.kt
+++ 30Dec2018/CLASS/RetrofitApp/app/src/main/java/android/ssuet/retrofitapp/Api.kt
@@ -4,14 +4,12 @@ import com.facebook.stetho.okhttp3.StethoInterceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.converter.scalars.ScalarsConverterFactory
 
 class Api {
     private val retrofit = Retrofit.Builder()
         .baseUrl("https://www.googleapis.com")
         .addConverterFactory(GsonConverterFactory.create())
-        //.addConverterFactory(ScalarsConverterFactory.create())
-        .client(OkHttpClient().newBuilder().addInterceptor(StethoInterceptor()).build())
+        .client(OkHttpClient.Builder().addNetworkInterceptor(StethoInterceptor()).build())
         .build()
 
     val service = retrofit.create(BooksApi::class.java)
